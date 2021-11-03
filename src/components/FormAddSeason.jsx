a87ba29a17678b85f7dbf58916de25d998a9ee39
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-11-03 13:23:11 +0100

    make all fields required

---
+++ src/components/FormAddSeason.jsx
@@ -0,0 +1,7 @@
+export default function FormAddSeason() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/FormAddShow.jsx
+++ src/components/FormAddShow.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import fieldsAddShow from '../data/fields-addShow.json'
-import InputField from './InputField'
+import { useState } from "react";
+import fieldsAddShow from "../data/fields-addShow.json";
+import InputField from "./InputField";
+import FormAddSeason from "./FormAddSeason";
 
 export default function FormAddShow() {
-
   const [values, setValues] = useState({});
 
   function onChange(key, value) {
@@ -11,34 +11,40 @@ export default function FormAddShow() {
     setValues({ ...values, ...imgField });
   }
 
-  function handleSubmit(event){
-      event.preventDefault()
-      console.log(values)
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(values);
   }
 
-  const InputFields = fieldsAddShow.map((input,index) => (
+  const InputFields = fieldsAddShow.map((input, index) => (
     <InputField key={index} options={input} onChange={onChange} />
   ));
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h1>Add show form</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Choose Category</label>
-        <select id="categories" name="category" onChange={(event)=>onChange('category', event.target.value)}>
-          <option value="serie">Serie</option>
-          <option value="movie">Movie</option>
-          <option value="documentary">Documentary</option>
-        </select>
-        {InputFields}
-        <label>Choose maturity</label>
-        <select id="maturity" name="maturityRating" onChange={(event)=>onChange('maturityRating', event.target.value)}>
-          <option value="7+">7+</option>
-          <option value="16+">16+</option>
-          <option value="18+">18+</option>
-        </select>
-        <button type='submit'>Submit</button>
-      </form>
-    </div>
+      <select
+        required
+        onChange={(event) => onChange("category", event.target.value)}
+      >
+        <option value="" selected disabled >Choose Category</option>
+        <option value="serie">Serie</option>
+        <option value="movie">Movie</option>
+        <option value="documentary">Documentary</option>
+      </select>
+      {InputFields}
+      <label>Choose maturity</label>
+      <select
+      required
+        onChange={(event) => onChange("maturityRating", event.target.value)}
+      >
+        <option value="" selected disabled >Choose Maturity Rating</option>
+        <option value="7+">7+</option>
+        <option value="16+">16+</option>
+        <option value="18+">18+</option>
+      </select>
+      <FormAddSeason />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
