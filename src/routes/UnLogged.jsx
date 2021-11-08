aa021a729a4497d7b662d95a38c8142eb8cc2922
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-11-08 18:17:54 +0100

    Added signup route

--- src/routes/UnLogged.jsx
+++ src/routes/UnLogged.jsx
@@ -3,11 +3,12 @@ import { Route } from "react-router-dom";
 
 //Project files
 import SignIn from "../pages/SignIn";
-
+import SignUp from '../pages/SignUp'
 export default function UnLogged({setIsLogged}) {
   return (
     <>
       <Route exact path="/" ><SignIn setIsLogged={setIsLogged}/></Route>
+      <Route path='/sign-up'><SignUp/></Route>
     </>
   );
 }
