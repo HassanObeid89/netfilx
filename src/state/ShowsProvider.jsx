749ad9dce1dde4b68b1e33265c6012a24a22e640
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-11-09 09:47:14 +0100

    Added show reducer

---
+++ src/state/ShowsProvider.jsx
@@ -0,0 +1,25 @@
+// NPM package
+import { createContext, useContext, useReducer } from "react";
+
+//Project files
+import courseReducer from "./showReducer";
+
+// Properties
+const UserContext = createContext(null);
+
+export function CoursesProvider({ children }) {
+  // Local state
+  const [courses, dispatchCourses] = useReducer(courseReducer, {});
+
+  return (
+    <UserContext.Provider
+      value={{ courses: courses, dispatchCourses: dispatchCourses }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useCourse() {
+  return useContext(UserContext);
+}
---
+++ src/state/showReducer.js
@@ -0,0 +1,34 @@
+export default function showReducer(state, action) {
+  switch (action.type) {
+    case "ADD_COURSE":
+      return addShow(state, action);
+    case "SET_SHOWS":
+      return setShows(state, action);
+    case "UPDATE_SHOW":
+      return updateShow(state, action);
+    default:
+      throw new Error("no action type found");
+  }
+
+  function addShow(state, action) {
+    const { payload } = action;
+    if (payload !== null) return [...state, payload];
+    return state;
+  }
+
+  function setShows(state, action) {
+    const { payload } = action;
+    if (payload !== null) return payload;
+    return state;
+  }
+
+  function updateShow(state, action) {
+    const { payload } = action;
+    const course = payload;
+    const newState = [...state];
+    const index = newState.findIndex((item) => item.id === course.id);
+
+    newState[index] = { ...course };
+    return newState;
+  }
+}
