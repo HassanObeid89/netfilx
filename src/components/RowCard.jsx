b4e38c998d98cd182d08d77c8226cf51b6e56c39
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-11-12 16:32:08 +0100

    clean up

--- src/components/RowCard.jsx
+++ src/components/RowCard.jsx
@@ -1,5 +1,5 @@
 export default function RowCard({show}) {
-    console.log(show)
+
     return (
         <li>
             <img src={show.imgUrl} alt=''/>
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -6,26 +6,18 @@ export default function HomeScreen() {
  const {shows} = useShow()
   const Movies = shows
     .filter((movie) => movie.category === "movies")
-    // .map((movie, index) => <Row key={index} data={movie} />);
 
   const Series = shows
     .filter((serie) => serie.category === "series")
-    // .map((serie, index) => <Row key={index} data={serie} />);
-
+    
   const Documentaries = shows
     .filter((documentary) => documentary.category === "documentaries")
-    // .map((documentary, index) => <Row key={index} data={documentary} />);
+    
 
   return (
     <div className='home_page'>
       <Banner series={Series}/>
       <section className='row'>
-      {/* <h2>Series</h2>
-      <ul>{Series}</ul>
-      <h2>Movies</h2>
-      <ul>{Movies}</ul>
-      <h2>Documentaries</h2>
-      <ul>{Documentaries}</ul> */}
       <Row data={Series}/>
       <Row data={Movies}/>
       <Row data={Documentaries}/>
